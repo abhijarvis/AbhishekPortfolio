866e2e1c77ae72a3949ed3c1cee86b86fc707212
abhijarvis <abhishekpandit740@gmail.com>
2025-03-05 22:21:49 +0530

    Updated Techstack

--- src/components/hero/TechStack.tsx
+++ src/components/hero/TechStack.tsx
@@ -20,7 +20,7 @@ const floatingVariants = (duration:number) => ({
 
 export const TechStack = () => {
   const technologies = [
-    { name: 'React', icon: <RiReactjsLine className="text-5xl text-cyan-400" />, delay: 3 },
+    { name: 'C', icon: <RiReactjsLine className="text-5xl text-cyan-400" />, delay: 3 },
     { name: 'JavaScript', icon: <FaJs className="text-5xl text-yellow-400" />, delay: 3.5 },
     { name: 'Tailwind', icon: <RiTailwindCssFill className="text-5xl text-blue-500" />, delay: 4 },
     { name: 'Node.js', icon: <FaNodeJs className="text-5xl text-green-500" />, delay: 3 },
